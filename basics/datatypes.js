fbbe20851f52d0b07fcf519fa73530166140327f
Shree-Spirit <gshridhar517@gmail.com>
2026-03-08 07:11:38 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,17 @@
+  "use strict"; // Treat all js code as newer version
+let name = "Shridhar"
+let age = 20
+
+// Number  --> 2 to power 53
+// String --> ""
+// bigint
+// boolean --> True or False
+// null --> Standalone value
+// undefined -->
+// Symbol --> For identifying uniqueness
+
+// Object 
+
+console.log(typeof null); //--> object
+
+console.log(typeof undefined); //--> undefined
